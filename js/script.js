d2f4ec0ad80d92b40c557927f8b26229cd2cb6db
stephanie sinet <stephaniesinet28@gmail.com>
2021-05-04 01:26:39 +0300

    add files

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
-function result (){
+function result(){
     var oneA = document.getElementById("1a");
-    var oneB =document.getElementById("1b");
-    var oneC =document.getElementById("1c");
+    var oneB = document.getElementById("1b");
+    var oneC = document.getElementById("1c");
     var twoA =document.getElementById("2a");
     var twoB =document.getElementById("2b");
     var twoC =document.getElementById("2c");
     var threeA =document.getElementById("3a");
     var threeB =document.getElementById("3b");
     var points=0;
+    var percentange;
 
     if(oneA.checked == true){
         points += 6;
     }
+    else if  (oneA.checked == false && oneB.checked == false && oneC.checked == false){
+        alert("please answer all questions")
+    }
     if(twoA.checked == true){
         points +=6;       
+    }
+    else if (twoA.checked == false && twoB.checked == false && twoC.checked == false)
+    {
+        alert("please answer all questions")
     }
     if(threeA.checked == true){
         points +=6;
     }
-    document.getElementById("answer").innerHTML=points;
+    else if  (threeA.checked == false && threeB.checked == false){
+        alert("please answer all questions")
+    }
+
+    percentange=(points/18)*100;
+
+    document.getElementById("answer").innerHTML="your total score is " + percentange +"%";
 }
