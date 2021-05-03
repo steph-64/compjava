32482f3975e02460013fb885a221240fabf1e568
stephanie sinet <stephaniesinet28@gmail.com>
2021-05-03 23:00:10 +0300

    add files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-function check() {
-    var (c=0);
-    var q1 document.quiz.question1 .value;
-    if (q1="")   
+function result (){
+    var oneA = document.getElementById("1a");
+    var oneB =document.getElementById("1b");
+    var oneC =document.getElementById("1c");
+    var twoA =document.getElementById("2a");
+    var twoB =document.getElementById("2b");
+    var twoC =document.getElementById("2c");
+    var threeA =document.getElementById("3a");
+    var threeB =document.getElementById("3b");
+    var points=0;
+
+    if(oneA.checked == true){
+        points += 6;
+    }
+    if(twoA.checked == true){
+        points +=6;       
+    }
+    if(threeA.checked == true){
+        points +=6;
+    }
+    document.getElementById("answer").innerHTML=points;
 }
